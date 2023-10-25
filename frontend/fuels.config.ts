bb9c964ebb2fd17e5ee631de22fc6111ffae2262
Daniel Bate <djbate23@gmail.com>
2023-10-25 13:18:10 +0100

    init

---
+++ frontend/fuels.config.ts
@@ -0,0 +1,11 @@
+import { createConfig } from 'fuels';
+
+export default createConfig({
+  workspace: '../sway-programs',
+  output: 'src/sway-programs-api',
+});
+
+/**
+ * Check the docs:
+ * https://fuellabs.github.io/fuels-ts/guide/cli/config-file
+ */
